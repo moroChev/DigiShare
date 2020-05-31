b73f9274066b73b9ea086253f41e2cbaef728c8a
rouessi <rouessi.mohcine@gmail.com>
2020-05-31 21:34:37 +0200

    addLocationToAgency route

--- codeSource/backend/routes/agencies.js
+++ codeSource/backend/routes/agencies.js
@@ -1,6 +1,7 @@
 const express                = require('express'),
       agencyController       = require('../controllers/agencies'),
-      auth                   = require('../middleware/auth');
+      auth                   = require('../middleware/auth'),
+      multer                 = require('../middleware/multer_config');
 
 let router = express.Router();
 
@@ -9,11 +10,12 @@ router.get("/:id", auth, agencyController.getAgencyById);
 
 //those routes will be modified later to add access restrictions
 //to allow only the staff in charge of getting the database initialized 
-router.post("/", auth, agencyController.createAgency);
+router.post("/", multer, agencyController.createAgency);
 router.post("/:idAgency/addSubsidiary_id::idSubsidiary",auth, agencyController.addSubsidiaryToAgency);
 
 //No need for this because it will be implemented inside signup
 //add an employee to an agency
-router.post("/:idAgency/addEmployee_id::idEmployee", agencyController.addEmployeeToAgency);
+router.post("/:idAgency/addEmployee_id::idEmployee", auth,agencyController.addEmployeeToAgency);
+router.post("/:id/addLocationToAgency", auth, agencyController.addLocationToAgency);
 
 module.exports = router;
