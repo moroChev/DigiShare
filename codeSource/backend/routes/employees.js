bfad88ddf81ede0f2e1ed735c41a85bc491dbb7b
rouessi <rouessi.mohcine@gmail.com>
2020-05-31 21:35:05 +0200

    modify employee route

--- codeSource/backend/routes/employees.js
+++ codeSource/backend/routes/employees.js
@@ -1,15 +1,17 @@
 const express                = require('express'),
       employeeController     = require('../controllers/employees.js'),
-      auth                   = require('../middleware/auth');
+      auth                   = require('../middleware/auth'),
+      multer                 = require('../middleware/multer_config');
 
 let router = express.Router();
 
 
 router.get("/rechercher/:fullName",auth, employeeController.getEmployeeByFullName);
-router.get("/", auth,employeeController.getAllEmployees);
-router.post("/", auth, employeeController.createEmployee);
-router.get("/:id",  auth,employeeController.getEmployeeById);
-router.get("/:id/Publications", auth,employeeController.getEmployeePublications);
+router.get("/", auth, employeeController.getAllEmployees);
+router.post("/", auth, multer, employeeController.createEmployee);
+router.get("/:id", auth, employeeController.getEmployeeById);
+router.get("/:id/Publications", auth, employeeController.getEmployeePublications);
+router.put("/:id", auth, multer, employeeController.modifyEmployee);
 
 
 
