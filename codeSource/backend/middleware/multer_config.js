d617b45e32b0c1a41ecac371f8c30aa69de3d2bc
rouessi <rouessi.mohcine@gmail.com>
2020-06-03 04:48:06 +0200

    filtre method test

--- codeSource/backend/middleware/multer_config.js
+++ codeSource/backend/middleware/multer_config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const util = require('util')
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -8,24 +9,32 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
+    console.log("destination /images ok ! and "+req);
     callback(null, './images');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     const fullName = name + Date.now() + '.' + extension;
+    console.log(util.inspect(file, false, null));
+    console.log("filename that will be sotred is"+fullName+" and the file is file :"+file);
+    
     callback(null, fullName);
   }
-});
+}
+);
 
-const fileFilter = (req,file,callback) => {
+/* const fileFilter = (req,file,callback) => {
   // in this method we want to filter the storage to store only the images files
+  console.log("file extension is "+file.mime);
   if( MIME_TYPES.hasOwnProperty(file.mimetype) )
   {
+    console.log("file extension is supported");
     callback(null, true);
   }else{
+    console.log("file extenstion isn't supported ...");
     callback(new Error({message: "this file format isn't accepted"}), false);
   }
-}
+} */
 
-module.exports = multer({ storage: storage, fileFilter: fileFilter}).single('imageUrl');
+module.exports = multer({ storage: storage}).single('imageUrl');
